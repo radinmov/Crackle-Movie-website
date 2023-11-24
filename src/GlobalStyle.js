abd039c447a4d334b06a220d3fefbef56d19a1b0
Radin Movahedi <125978983+radinmov@users.noreply.github.com>
2023-11-24 13:28:00 +0330

    Style Update 1.0.1

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,10 +1,71 @@
 import { createGlobalStyle } from "styled-components";
-import { pallete } from "./theme";
 
-export const GlobalStyle =createGlobalStyle `
-body{
-    margin:0;
-    font-family:'Sora';
+export const GlobalStyle = createGlobalStyle`
+body, button, input {
+    font-family: Sora;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
 }
-
+body {
+    margin: 0;
+    direction: ltr;
+    font-family: 'Sora';
+    background: #000;
+    color: #fff;
+}
+ul , ol {
+    padding: 0;
+    margin: 0;
+    list-style: none;
+}
+*, *:after, *:before {
+    box-sizing: border-box;
+}
+a {
+    text-decoration: none;
+}
+.full-container {
+   width: 100%;
+   padding: 0 16px;
+}
+.container {
+   width: 100%;
+   padding: 0 72px;
+}
+.h-full {
+    height: 100%;
+}
+.relative {
+    position: relative;
+}
+.flex {
+    display: flex;
+}
+.space-between {
+    justify-content: space-between
+}
+.justify-center {
+    justify-content: center
+}
+.flex-wrap {
+    flex-wrap: wrap;
+}
+.align-center {
+    align-items: center;
+}
+.gap-5 {
+    gap: 5px
+}
+.gap-10 {
+    gap: 10px
+}
+.gap-15 {
+    gap: 15px
+}
+.gap-20 {
+    gap: 20px;
+}
+.gap-40 {
+    gap: 40px;
+}
 `;
---
+++ src/componets/uiElements/Button/index.js
@@ -0,0 +1,27 @@
+import { Fragment } from "react";
+
+export default function Button(props) {
+    const {
+        children,
+        className,
+        size = "medium"
+    }=props;
+    function renderFarm() {
+        if (icon) {
+            return (
+                <div className="flex align-center gap-5">
+                    <span>
+                        <i className={icon} />
+                    </span>
+                    <span>{children}</span>
+                </div>
+            )
+        } else {
+            return <Fragment>{children}</Fragment>
+        }
+    }
+    return  (
+        <sty
+    )
+
+}
