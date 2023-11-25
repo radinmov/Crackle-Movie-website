31a0920a0fb409a74cad754a20e892017a89f507
Radin Movahedi <125978983+radinmov@users.noreply.github.com>
2023-11-25 21:13:54 +0330

    2.1.1

--- src/componets/Header/style.js
+++ src/componets/Header/style.js
@@ -2,5 +2,28 @@ import styled from "styled-components";
 import { pallete } from "../../theme";
 
  export const Style = styled.div `
-
+ background: rgba(0, 0, 0, 0.5);
+ padding: 10px 0;
+ position: fixed;
+ top: 0;
+ left: 0;
+ width: 100%;
+ z-index: 10;
+ backdrop-filter: blur(20px);
+ .logo {
+   img {
+     width: 109px;
+     height: auto;
+   }
+ }
+ .menu {
+   ul {
+     li {
+       a {
+         display: flex;
+         gap: 5px;
+       }
+     }
+   }
+ }
 `;
