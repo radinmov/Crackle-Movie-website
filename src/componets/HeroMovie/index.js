bcd4015cdba7d194330b242f14569c7c23036d23
Radin Movahedi <125978983+radinmov@users.noreply.github.com>
2023-11-26 19:35:38 +0330

    2.1.2

--- src/componets/HeroMovie/index.js
+++ src/componets/HeroMovie/index.js
@@ -1,32 +1,35 @@
+import Button from "../uiElements/Button";
 import { Style } from "./style";
 
-
-export default function HeroMovie({data}) {
+export default function HeroMovie({ data }) {
   const { poster, title, age, genres, playLink, infoLink, description } = data;
-    return (
+  return (
     <Style>
-        <div className="hero-movie">
-            <img className="poster" src={poster}/>
-            <div className="content flex align-center h-full relative">
-                <div className="container">
-                    <h1 className="name">{title}</h1>
-                    <div className="flex gap-10 align-center">
-                        <div className="age">{`TV-${age}`}</div>
-                        <div className="genres">{genres.join(" . ")}</div>
-                    </div>
-                    <div className="flex gap-10">
-                        <div className="btn-group flex gap-10">
-                            {/* <Button>watch Now</Button> */}
-                            {/* <Button>More Info</Button> */}
-                        </div>
-                    </div>
-                    <div className="description">
-                        <p>{description}</p>
-                    </div>
-                </div>
+      <div className="hero-movie">
+        <img className="poster" src={poster} />
+        <div className="content flex align-center h-full relative">
+          <div className="container">
+            <h1 className="name">{title}</h1>
+            <div className="flex gap-10 align-center">
+              <div className="age">{`TV-${age}`}</div>
+              <div className="genres">{genres.join(" . ")}</div>
             </div>
+            <div className="flex gap-10">
+              <div className="btn-group flex gap-10">
+                <Button icon="fa-solid fa-play" size="large" type="primary">
+                  Watch Now
+                </Button>
+                <Button size="large" type="dark">
+                  More Info
+                </Button>
+              </div>
+            </div>
+            <div className="description">
+              <p>{description}</p>
+            </div>
+          </div>
         </div>
+      </div>
     </Style>
-
-    );
+  );
 }
