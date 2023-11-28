97a0d7467cbfdaa35a15fce9d70b803f722dd290
Radin Movahedi <125978983+radinmov@users.noreply.github.com>
2023-11-28 14:45:44 +0330

    2.3.2

--- src/Pages/Search/index.js
+++ src/Pages/Search/index.js
@@ -4,18 +4,33 @@ import { Style } from "./style";
 import { api } from "../../utils/api";
 
 export default function Search() {
-    const [data,setDate]=useState({
-        data:[],
-        metadata:{
-            current_page: 1,
-            per_page: 1,
-            page_count: 10,
-            total_count: 250
-        }
-    })
+  const [data, setData] = useState({
+    data: [],
+    metadata: {
+      current_page: 1,
+      per_page: 1,
+      page_count: 10,
+      total_count: 250,
+    },
+  });
   function typing(event) {
     console.log(event.target.value);
-    api.get("movies" , {params:{q:event.target.value}})
+    api
+      .get("movies", { params: { q: event.target.value } })
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch(function (error) {});
+  }
+  function renderFarm() {
+    return data.data.map(({ id, poster, title }) => {
+      return (
+        <li key={id}>
+          <h2>{title}</h2>
+          <img src={poster} />
+        </li>
+      );
+    });
   }
 
   return (
@@ -30,6 +45,7 @@ export default function Search() {
             />
           </div>
         </div>
+        <ul>{renderFarm()}</ul>
       </Style>
     </PrimaryLayout>
   );
--- src/Pages/Search/style.js
+++ src/Pages/Search/style.js
@@ -1,5 +1,12 @@
 import styled from "styled-components";
 
 export const Style = styled.div`
-
+input
+{
+outline: 0px;
+  width: 100%;
+  border: none;
+  color: #fff;
+  background: transparent;
+}
 `;
